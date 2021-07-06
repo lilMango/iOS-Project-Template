230e88e04908957ff26726cd0b58dbc0b7e67932
Miguel <miguelpaysan@gmail.com>
2021-07-05 20:53:09 -0700

    Initial Commit

---
+++ Project-Template/Project_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_TemplateApp.swift
+//  Project-Template
+//
+//  Created by Miguel Paysan on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Project_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
